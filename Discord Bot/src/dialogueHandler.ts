59fffe43bec431206a7b0c293f3ced602a3cce07
Mick Rustenhoven <rustenhovenmick@outlook.com>
2018-08-22 23:57:36 +0200

    Testing out dialogueHandler, added a dialogueStep to support even better dialogues

--- Discord Bot/src/dialogueHandler.ts
+++ Discord Bot/src/dialogueHandler.ts
@@ -1,18 +1,48 @@
 import * as discord from 'discord.js'
 
 export class dialogueHandler {
+    private _steps: dialogueStep[] | dialogueStep;
+
+    /**
+     *
+     */
+    constructor(steps: dialogueStep[] | dialogueStep) {
+        this._steps = steps;
+    }
 
     public async GetInput(channel: discord.TextChannel) {
+        // Create array for single dialogueStep to prevent extra checks + coding
+        if (!Array.isArray(this._steps)) {
+            this._steps = [this._steps];
+        }
 
-        const filter = m => m;
+        this._steps.forEach(step => {
+            const filter = m => m;
 
-        let response;
+            let response;
 
-        channel.awaitMessages(filter, {max: 1, time: 5000, errors: ['time']})
-            .then(collected => response = collected.array()[0])
-            .catch(collected => channel.send("You didn't respond in time!"))
-            
-        return response;
+            channel.awaitMessages(filter, { max: 1, time: 5000, errors: ['time'] })
+                .then(collected => {
+                    response = collected.array()[0];
+                    
+                    if(step.callback!= undefined)
+                        step.callback(response);
+                    if(step.httpCallback!= undefined)
+                        step.httpCallback(response);
+                })
+                .catch(collected => channel.send("You didn't respond in time!"))
+
+             
+        });
+
+
+        return;
 
     }
+}
+
+export interface dialogueStep {
+    callback?: Function;
+    data: any;
+    httpCallback?: Function;
 }
